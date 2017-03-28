21c11a071b7fd7d958c82b0dec94449b35dafe84
itggot-Anna-Nilsson <anna.nilsson@itggot.se>
2017-03-28 13:54:53 +0200

    Added a shadow
    En skugga följer med när man klickar på menyn

--- site/public/js/function.js
+++ site/public/js/function.js
@@ -1,5 +1,16 @@
 
 function slide(){
+  var darkbox = document.querySelector("#darkbox");
+  if(!darkbox.classList.contains('opacity50') && !darkbox.classList.contains('opacity0')){
+    darkbox.classList.toggle('opacity0');
+    darkbox.classList.remove('darkbox');
+  }
+  else {
+    darkbox.classList.toggle('opacity0');
+    darkbox.classList.toggle('opacity50');
+    darkbox.classList.remove('darkbox');
+  }
+
   var element = document.querySelector("#navi");
   console.log("slide");
   if(!element.classList.contains('menuhidden') && !element.classList.contains('menuvisible')){
