504a51ccc45d6e4612dc98c8d69f5d6f2f9e74f7
itggot-Anna-Nilsson <anna.nilsson@itggot.se>
2017-04-04 15:02:30 +0200

    Fixed centering and added a responsive title

--- site/public/js/function.js
+++ site/public/js/function.js
@@ -26,3 +26,19 @@ var x = location.pathname.split('/');
 var y = x[x.length - 1];
 var z = document.querySelector("a[href='" + y + "']");
 z.classList.add('current_page');
+
+if(y == 'favorites.html'){
+  document.querySelector('#title').innerHTML = 'Favorites';
+}
+else if(y == 'maps.html'){
+  document.querySelector('#title').innerHTML = 'Map';
+}
+else if(y == 'add_location.html'){
+  document.querySelector('#title').innerHTML = 'Add Location';
+}
+else if(y == 'recent.html'){
+  document.querySelector('#title').innerHTML = 'Recent';
+}
+else if(y == 'settings.html'){
+  document.querySelector('#title').innerHTML = 'Settings';
+}
